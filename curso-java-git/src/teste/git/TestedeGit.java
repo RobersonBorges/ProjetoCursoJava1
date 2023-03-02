63bfd328b725ea89bbf75e6f7a7021661af40fa8
RobersonBorges <robersonfborges@gmail.com>
2023-03-01 21:31:29 -0300

    mais um teste de commit

--- curso-java-git/src/teste/git/TestedeGit.java
+++ curso-java-git/src/teste/git/TestedeGit.java
@@ -1,8 +1,9 @@
 package teste.git;
-
+/* testes */
 public class TestedeGit {
 	public static void main(String[] args) {
-		System.out.println("testando print");
+		System.out.println("testando print estamos digitando");
+		System.out.println("mais um teste de print");
 	}
 
 }
